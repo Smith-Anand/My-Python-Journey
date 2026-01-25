d64744f1f93fae79bc980c6367fced1bbf81c0c2
smith anand <smithanand5@gmail.com>
2026-01-25 22:43:37 +0530

    LeetCodeProblemNo7

---
+++ LeetCodeProblem7.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+obj = Solution()
+print(obj.twoSum([2, 7, 11, 15], 9))
